7d97d92f485922a50cadd4e2729eff0b32c1ff46
Vincent Mahnke <info@vimaster.de>
2018-10-12 22:18:20 +0200

    Added tester script to validate communication with server

---
+++ 0_unity/Assets/Scripts/Tester/WebSocketServerTester.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+
+public class WebSocketServerTester : MonoBehaviour {
+
+	private WebSocket websocketClient;
+
+	void Start ()
+	{
+		websocketClient = new WebSocket("ws://projectsimulatorsimulator.herokuapp.com/:39759");
+		websocketClient.OnOpen += OnOpen;
+		websocketClient.Connect ();
+		websocketClient.Send ("{\"command\":\"createSession\"}");
+	}
+
+	void OnOpen(object sender, System.EventArgs e)
+	{
+		websocketClient.Send ("{\"command\":\"updateCarPosition\", \"sessionDI\": 0, \"x\": 10, \"y\": 20}");
+	}
+	
+}
