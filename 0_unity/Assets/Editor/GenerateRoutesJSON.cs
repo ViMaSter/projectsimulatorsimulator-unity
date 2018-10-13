c21a62b51bcc3fb91afdca0d6cff2773fc82a2ff
Vincent Mahnke <info@vimaster.de>
2018-10-13 12:56:27 +0200

    Added routes to TaxiScene Added editor window to generate MapData from Unity Added first set of MapData

---
+++ 0_unity/Assets/Editor/GenerateRoutesJSON.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEditor;
+
+[System.Serializable]
+public class Route
+{
+    public Vector2 start;
+    public Vector2 end;
+    public Route(Vector2 start, Vector2 end)
+    {
+		this.start = start;
+		this.end = end;
+    }
+}
+
+[System.Serializable]
+public class Scene
+{
+	public List<Route> routes;
+	public Scene(List<Route> routes)
+	{
+		this.routes = routes;
+	}
+}
+
+
+public class GenerateRoutesJSON : EditorWindow
+{
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/SimulatorSimulator/Generate Route JSON")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        GenerateRoutesJSON window = (GenerateRoutesJSON)EditorWindow.GetWindow(typeof(GenerateRoutesJSON));
+        window.Show();
+    }
+
+	void OnGUI () {
+		if(GUILayout.Button("Generate JSON for scene"))
+        {
+            var path = EditorUtility.SaveFilePanel(
+                "Save Route JSON",
+                Path.Combine(Path.Combine(Application.dataPath, "Scenes"), "MapData"),
+                EditorSceneManager.GetActiveScene().name + ".json",
+                "json"
+            );
+
+            string json = GeneratePathJSON();
+
+            File.WriteAllBytes(path, System.Text.Encoding.UTF8.GetBytes(json));
+        }
+	}
+
+	string GeneratePathJSON()
+	{
+		Transform rootTransform = GameObject.Find("Routes").GetComponent<Transform>();
+		List<Route> routes = new List<Route>(rootTransform.childCount);
+		foreach (Transform child in rootTransform)
+		{
+			Transform startTransform = child.Find("Start").GetComponent<Transform>();
+			Transform endTransform = child.Find("End").GetComponent<Transform>();
+			routes.Add(new Route(startTransform.position, endTransform.position));
+		}
+		Scene scene = new Scene(routes);
+		return JsonUtility.ToJson(scene);
+	}
+}
---
+++ 0_unity/Assets/Scripts/DrawRouteGizmos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawRouteGizmos : MonoBehaviour
+{
+	Transform startTransform;
+	Transform endTransform;
+	void Awake()
+	{
+		Transform thisTransform = GetComponent<Transform>();
+		startTransform = thisTransform.Find("Start").GetComponent<Transform>();
+		endTransform = thisTransform.Find("End").GetComponent<Transform>();
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(startTransform.position, 1);
+		Gizmos.color = Color.green;
+		Gizmos.DrawSphere(endTransform.position, 1);
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawLine(startTransform.position, endTransform.position);
+	}
+
+}
